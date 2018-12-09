372519eb03e1a2061d17843fcf958773e95859b4
Rajat Meena <rajat8527@gmail.com>
2018-12-10 00:26:44 +0530

    Added code to display free and taken parking slots

--- src/com/cpm/Main.java
+++ src/com/cpm/Main.java
@@ -18,7 +18,7 @@ public class Main {
         String output = "";
         String result = "";
 
-        String input = "pABC,pDEF,pGHI,u5000,c,pXYZ,pLOM,pGSI,pWHI,pJHI,pMHI,u5005,c,pGHI,pGHX,pGHW";
+        String input = "pABC,pDEF,pGHI,u5000,c";
 
         String[] splitLicensePlate = input.split( "," );
 
@@ -27,6 +27,7 @@ public class Main {
         do{
             if(splitLicensePlate[i].startsWith( "p" )){
                 String[] moreSplit = splitLicensePlate[i].split( "p" );
+                parkingSlots[i] = String.valueOf( ticketNo );
                 carDataMap.put(ticketNo,moreSplit[1]);
                 carDataMapClone.put(ticketNo,moreSplit[1]);
                 ticketNo++;
@@ -41,13 +42,27 @@ public class Main {
                 }
             }
             i++;
-        }while (i<parkingSlots.length);
+        }while (i<splitLicensePlate.length);
 
         for (String values : carDataMap.values()) {
             result += values + ",";
             output = result.replace( "p","" );
         }
+        String freeSpace = "";
+        String takenSpace = "";
+        for(int w=0; w<parkingSlots.length; w++){
+            if((parkingSlots[w] == null) || (parkingSlots[w].trim().length() == 0)){
+                freeSpace += "Slot" + " " + String.valueOf(w) + ",";
+            }
+            else{
+                takenSpace += "Slot" + " " + String.valueOf(w) + ",";
+            }
+        }
 
+        String spaceStatus = freeSpace + takenSpace;
+
+        System.out.println( "Free Parking Slot => " + " " + freeSpace );
+        System.out.println( "Taken Parking Slot => " + " " + takenSpace );
         System.out.println( "Car Parking Map (Before) => " + " " + carDataMapClone );
         System.out.println( "Car Parking Map (After)  => " + " " + carDataMap );
         System.out.println( "Input => " + " " + input );
