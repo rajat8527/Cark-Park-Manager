c328657fcb930e8f75132c9aaf6efaf2b86a5001
Rajat Meena <rajat8527@gmail.com>
2018-12-10 03:35:42 +0530

    Added comments to the code

--- src/com/cpm/Main.java
+++ src/com/cpm/Main.java
@@ -6,62 +6,69 @@ public class Main {
 
     public static void main(String[] args) {
 
-        HashMap<Integer,String> carDataMap = new HashMap<Integer,String>(  );
-        HashMap<Integer,String> carDataMapClone = new HashMap<Integer,String>(  );
+        //Map to store parked car data against its ticket number
+        HashMap<Integer, String> carDataMap = new HashMap<Integer, String>();
+        //Clone of the above map to show the changes in the map
+        HashMap<Integer, String> carDataMapClone = new HashMap<Integer, String>();
 
-        ArrayList<String> carDataList = new ArrayList<>(  );
+        //Array List to track parking slots availability
+        ArrayList<String> parkingSlots = new ArrayList<>( Arrays.asList( new String[10] ) );
 
-
-        ArrayList<String> parkingSlots = new ArrayList<>(Arrays.asList(new String[11]));
-
+        //ticket number initialized at 5000
         int ticketNo = 5000;
+
+        //final output string
         String output = "";
         String result = "";
+        String freeSpace = "";
+        String takenSpace = "";
 
+        //taking user input
         Scanner sc = new Scanner( System.in );
-
         System.out.println( "Enter an input string to process ....\n" );
+        //storing the input string
         String input = sc.nextLine();
+
 
         String[] splitLicensePlate = input.split( "," );
 
         int i = 0;
 
-        do{
-            if(splitLicensePlate[i].startsWith( "p" )){
-                String[] moreSplit = splitLicensePlate[i].split( "p" );
-                parkingSlots.set(i,String.valueOf(ticketNo));
-                carDataMap.put(ticketNo,moreSplit[1]);
-                carDataMapClone.put(ticketNo,moreSplit[1]);
-                ticketNo++;
-            }
-            if(splitLicensePlate[i].startsWith( "u" ) && splitLicensePlate[i+1].equalsIgnoreCase( "c" )){
-                String[] moreSplit = splitLicensePlate[i].split( "u" );
-                int key = Integer.parseInt( moreSplit[1] );
-                if(carDataMap.containsKey(key)){
-                    parkingSlots.remove( String.valueOf( key ) );
-                    carDataMap.remove(key);
-                } else {
-                    System.out.println("Car with ticket no"+" "+key+" "+"is not parked here !");
+        try {
+            do {
+                if ( splitLicensePlate[i].startsWith( "p" ) ) {
+                    String[] licensePlate = splitLicensePlate[i].split( "p" );
+                    parkingSlots.set( i, String.valueOf( ticketNo ) );
+                    carDataMap.put( ticketNo, licensePlate[1] );
+                    carDataMapClone.put( ticketNo, licensePlate[1] );
+                    ticketNo++;
                 }
-            }
-            i++;
-        }while (i<splitLicensePlate.length);
+                if ( splitLicensePlate[i].startsWith( "u" ) && splitLicensePlate[i + 1].equalsIgnoreCase( "c" ) ) {
+                    String[] licensePlate = splitLicensePlate[i].split( "u" );
+                    int key = Integer.parseInt( licensePlate[1] );
+                    if ( carDataMap.containsKey( key ) ) {
+                        parkingSlots.remove( String.valueOf( key ) );
+                        carDataMap.remove( key );
+                    } else {
+                        System.out.println( "Car with ticket no" + " " + key + " " + " not found !" );
+                    }
+                }
+                i++;
+            } while (i < splitLicensePlate.length);
+        } catch (Exception e) {
+            throw e;
+        }
 
         for (String values : carDataMap.values()) {
             result += values + ",";
-            output = result.replace( "p","" );
+            output = result.replace( "p", "" );
         }
 
-        String freeSpace = "";
-        String takenSpace = "";
-
-        for(int w=0; w<parkingSlots.size(); w++){
-            if((parkingSlots.get( w ) == null )){
-                freeSpace += "Slot" + " " + String.valueOf(w) + ",";
-            }
-            else{
-                takenSpace += "Slot" + " " + String.valueOf(w) + ",";
+        for (int w = 0; w < parkingSlots.size(); w++) {
+            if ( (parkingSlots.get( w ) == null) ) {
+                freeSpace += "Slot" + " " + String.valueOf( w ) + ",";
+            } else {
+                takenSpace += "Slot" + " " + String.valueOf( w ) + ",";
             }
         }
 
